f70207c8e9f91498c59c56700b196c6c553f3e73
yakupdurmus <yakup.durmus@turkcell.com.tr>
2021-08-08 13:32:13 +0300

    config edited

--- yakupdurmus/docusaurus.config.js
+++ yakupdurmus/docusaurus.config.js
@@ -3,8 +3,8 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Yakup Durmuş',
+  tagline: 'Documents web site',
   url: 'https://yakupdurmus.github.io/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
@@ -14,7 +14,7 @@ module.exports = {
   projectName: 'yakupdurmus.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
-      title: 'My Site',
+      title: 'Yakup Durmuş',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
